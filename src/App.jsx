3649497476bc4a55222628bb13c25ed1b89637ef
MohiniMehta <mohinimehta12@gmail.com>
2024-03-19 20:20:38 +0530

    added my logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,28 +1,27 @@
 import { BrowserRouter } from "react-router-dom";
-import {About,  Contact, Experience, Feedbacks, Hero, Navbar, Tech, Work, StarCanvas} from './components';
+import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works } from './components';
 
 
-const App=()=> {
-
+const App = () => {
   return (
     <BrowserRouter>
-    <div className="relative z-0 bg-primary">
-     <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
-      <Navbar/>
-      <Hero/>
-     </div>
-     <About/>
-     <Experience/>
-     <Tech/>
-     <Work/>
-     <Feedbacks/>
-     <div className="relative-z-0">
-      <Contact/>
-      <StarCanvas/>
-     </div>
-    </div>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar />
+          <Hero />
+        </div>
+        <About />
+        <Experience />
+        <Tech />
+        <Works />
+        <Feedbacks />
+        <div className="relative-z-0">
+          <Contact />
+         
+        </div>
+      </div>
     </BrowserRouter>
   )
 }
 
-export default App
+export default App;
