17f84c16659dfe4146f150b05359bf4de62a6826
MohiniMehta <mohinimehta12@gmail.com>
2024-03-20 15:37:25 +0530

    created about section

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,41 +1,68 @@
-import React from 'react'
-// import Tilt from 'react-tilt';
-import {motion} from 'framer-motion';
-import { styles } from '../styles';
-import { services } from '../constants';
-import {fadeIn, textVariant} from '../utils/motion';
+import React from "react";
+import Tilt from "react-tilt";
+import { motion } from "framer-motion";
 
-const ServiceCard=({index,title,icon})=>{
-return(
-  <p>{service.title}</p>
-)
-}
+import { styles } from "../styles";
+import { services } from "../constants";
+import { SectionWrapper } from "../hoc";
+import { fadeIn, textVariant } from "../utils/motion";
+
+const ServiceCard = ({ index, title, icon }) => (
+  <Tilt className='xs:w-[250px] w-full'>
+    <motion.div
+      variants={fadeIn("right", "spring", index * 0.5, 0.75)}
+      className='w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card'
+    >
+      <div
+        options={{
+          max: 45,
+          scale: 1,
+          speed: 450,
+        }}
+        className='bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col'
+      >
+        <img
+          src={icon}
+          alt='web-development'
+          className='w-16 h-16 object-contain'
+        />
+
+        <h3 className='text-white text-[20px] font-bold text-center'>
+          {title}
+        </h3>
+      </div>
+    </motion.div>
+  </Tilt>
+);
 
 const About = () => {
   return (
     <>
-    <motion.div variants={textVariant()}>
-      <p className={styles.sectionSubText}>
-        Introduction
-      </p>
-      <h2 className={styles.sectionHeadText}>Overview</h2>
-    </motion.div>
-    <motion.p variants={fadeIn("","",0.1,1)}
-    className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'>
-    I'm Mohini Mehta, a passionate and creative web developer from Pimpri Chinchwad College of Engineering. I specialize in web development and app development, with a strong focus on creating user-friendly and visually appealing websites.
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Introduction</p>
+        <h2 className={styles.sectionHeadText}>Overview.</h2>
+      </motion.div>
+
+      <motion.p
+        variants={fadeIn("", "", 0.1, 1)}
+        className='mt-4 text-secondary text-[17px] max-w-3xl leading-[30px]'
+      >
+       
+I'm Mohini Mehta, a passionate and creative web developer from Pimpri Chinchwad College of Engineering. I specialize in web development and app development, with a strong focus on creating user-friendly and visually appealing websites.
 
 My journey into the world of web development began at Pimpri Chinchwad College of Engineering, where I discovered my love for coding and design. Since then, I have been on a continuous learning journey, exploring various technologies and honing my skills to deliver high-quality solutions.
 
 Some of my key skills include HTML, CSS, JavaScript, and Flutter. I enjoy the process of turning concepts into reality through the power of code. Whether it's crafting a seamless user interface or developing innovative applications, I am always excited about taking on new challenges.
  
-    </motion.p>
-    <div className='mt-20 flex flex-wrap gap-10'>
-      {services.map((service, index)=>{
-        <ServiceCard key={service.title} index={index} {...service}/>
-      })}
-    </div>
-    </>
-  )
-}
+     </motion.p>
 
-export default About
+      <div className='mt-20 flex flex-wrap gap-10'>
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
+      </div>
+    </>
+  );
+};
+
+export default SectionWrapper(About, "about");
