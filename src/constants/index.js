507484e27971b19982b8cca6fcbf51a099bd8336
MohiniMehta <mohinimehta12@gmail.com>
2024-04-05 21:25:48 +0530

    updated timeline

--- src/constants/index.js
+++ src/constants/index.js
@@ -157,39 +157,14 @@ import {
       company_name: "Pimpri Chinchwad College of Engineering",
       icon: meta,
       iconBg: "#E6DEDD",
-      date: "June 2023 - July 2023",
+      date: "",
       points: [
-        "Published research paper",
+        "",
       ],
     },
   ];
   
-  const testimonials = [
-    {
-      testimonial:
-        "I thought it was impossible to make a website as beautiful as our product, but Rick proved me wrong.",
-      name: "Sara Lee",
-      designation: "CFO",
-      company: "Acme Co",
-      image: "https://randomuser.me/api/portraits/women/4.jpg",
-    },
-    {
-      testimonial:
-        "I've never met a web developer who truly cares about their clients' success like Rick does.",
-      name: "Chris Brown",
-      designation: "COO",
-      company: "DEF Corp",
-      image: "https://randomuser.me/api/portraits/men/5.jpg",
-    },
-    {
-      testimonial:
-        "After Rick optimized our website, our traffic increased by 50%. We can't thank them enough!",
-      name: "Lisa Wang",
-      designation: "CTO",
-      company: "456 Enterprises",
-      image: "https://randomuser.me/api/portraits/women/6.jpg",
-    },
-  ];
+ 
   
   const projects = [
     {
@@ -313,4 +288,4 @@ import {
     },
   ];
   
-  export { services, technologies, experiences, testimonials, projects };
+  export { services, technologies, experiences, projects };
